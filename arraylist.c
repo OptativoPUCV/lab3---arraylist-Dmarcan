6936fa92262560fa5f8b1a4bb49f2ec7b1149e0e
Replit user <>
2023-03-29 22:56:42 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -11,8 +11,8 @@ typedef struct ArrayList {
 } ArrayList;
 
 ArrayList *createList(void) {
-  //ArrayList *list=malloc(sizeof(ArrayList));
-  //if (list==NULL) exit(EXIT_FAILURE);
+  ArrayList *list=malloc(sizeof(ArrayList));
+  if (list==NULL) exit(EXIT_FAILURE);
 }
 
 void append(ArrayList * l, void * data){
